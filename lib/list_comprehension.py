19dc04de5648855a6e5c3ebff0f53881d82ca271
Luis <foreverluiscastro@gmail.com>
2022-07-09 19:17:10 -0400

    Done.

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+from logging import makeLogRecord
+import ipdb
+
 def return_evens(num_list):
     pass
+    even_list = [n for n in num_list if n % 2 == 0]
+    # ipdb.set_trace()
+    return even_list
+
+return_evens([1,3,5])
 
 def make_exclamation(sentence_list):
     pass
+    # ipdb.set_trace()
+    exclamationed_strings = [str + "!" for str in sentence_list]
+    return exclamationed_strings
+
+make_exclamation(["Your", "Mom's", "House"])
